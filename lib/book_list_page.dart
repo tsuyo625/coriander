c36d2d09fd834615454127e672240ad2112943e2
Tsuyoshi Ishitani <tsuyoshi.gt@gmail.com>
2020-11-09 02:21:19 +0900

    BookModel追加

---
+++ lib/book_list_page.dart
@@ -0,0 +1,30 @@
+import 'package:coriander/book_list_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BookListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<BookListModel>(
+      create: (_) => BookListModel()..fetchBooks(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Book'),
+        ),
+        body: Consumer<BookListModel>(
+          builder: (context, model, child) {
+            final books = model.books;
+            final listTiles = books
+                .map(
+                  (book) => ListTile(
+                    title: Text(book.title),
+                  ),
+                )
+                .toList();
+            return ListView(children: listTiles);
+          },
+        ),
+      ),
+    );
+  }
+}
